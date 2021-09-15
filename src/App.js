f4050c0e12a039676c60049c6a031c0f9243cbb7
Daniel Laloush <daniel.laloush@influitive.com>
2021-09-15 13:32:43 -0400

    temp design

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
+import whatsapp from './whatsapp.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="main">
+      <header className="header">
+        <img src={logo} className="logo" alt="logo" />
       </header>
+      <main>
+        <h1>Contáctanos Ya!</h1>
+        <a href="https://wa.link/po5ua3" className="whatsapp" title="+58 424 418 2546">
+          <img src={whatsapp} alt="+58 424 418 2546" />
+          <span><strong>+58 424 418 2546</strong></span>
+        </a>
+        <a href="https://wa.link/oq2d6w" className="whatsapp" title="+58 412 246 0232">
+          <img src={whatsapp} alt="+58 412 246 0232" />
+          <span><strong>+58 412 246 0232</strong></span>
+        </a>
+      </main>
     </div>
   );
 }
